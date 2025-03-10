209b8563b170bdb4af78b85013121b4208a3894e
Cielo Angulo <cielodamarisangulo@gmail.com>
2025-03-10 01:32:39 -0500

    se carga material.zip

--- src/config/global.js
+++ src/config/global.js
@@ -98,7 +98,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/86120363_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
